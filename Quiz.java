457a674464413fcbc3e9d47d5fb23a74cee37668
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-07-19 21:30:16 +0600

    add img, 2D arr - ques, ans, userAns. BTN, timer and correlation

---
+++ Quiz.java
@@ -0,0 +1,323 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
+public class Quiz extends JFrame implements ActionListener{
+
+    //putting the questions, answers in two individual 2D array
+    String[][] questions = new String[10][5];
+    String[][] answers = new String[10][2];
+    String[][] userAns = new String[10][1];
+    public static int count = 0;
+
+    // globally declaring to give access to other methods
+    JLabel quesNum, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupOptions;
+
+    JButton next, submit, hint;
+
+    public static int timer = 5;
+    public static int ansGiven = 0;
+
+    Quiz(){
+        setBounds(250, 0, 1440, 1080);
+        getContentPane().setBackground(Color.white);
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Images/quiz.jpg"));
+
+        // setting image for login
+
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+
+        //question numbers
+        quesNum = new JLabel();
+        quesNum.setBounds(100, 450, 50, 30);
+        quesNum.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(quesNum);
+
+        // questions 
+        question = new JLabel();
+        question.setBounds(150, 450, 800, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(question);
+
+        //manually adding all the elements in the 2D arr [Questions]
+        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
+        questions[0][1] = "JVM";
+        questions[0][2] = "JDB";
+        questions[0][3] = "JDK";
+        questions[0][4] = "JRE";
+
+        questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
+        questions[1][1] = "int";
+        questions[1][2] = "Object";
+        questions[1][3] = "long";
+        questions[1][4] = "void";
+
+        questions[2][0] = "Which package contains the Random class?";
+        questions[2][1] = "java.util package";
+        questions[2][2] = "java.lang package";
+        questions[2][3] = "java.awt package";
+        questions[2][4] = "java.io package";
+
+        questions[3][0] = "An interface with no fields or methods is known as?";
+        questions[3][1] = "Runnable Interface";
+        questions[3][2] = "Abstract Interface";
+        questions[3][3] = "Marker Interface";
+        questions[3][4] = "CharSequence Interface";
+
+        questions[4][0] = "In which memory a String is stored, when we create a string using new operator?";
+        questions[4][1] = "Stack";
+        questions[4][2] = "String memory";
+        questions[4][3] = "Random storage space";
+        questions[4][4] = "Heap memory";
+
+        questions[5][0] = "Which of the following is a marker interface?";
+        questions[5][1] = "Runnable interface";
+        questions[5][2] = "Remote interface";
+        questions[5][3] = "Readable interface";
+        questions[5][4] = "Result interface";
+
+        questions[6][0] = "Which keyword is used for accessing the features of a package?";
+        questions[6][1] = "import";
+        questions[6][2] = "package";
+        questions[6][3] = "extends";
+        questions[6][4] = "export";
+
+        questions[7][0] = "In java, jar stands for?";
+        questions[7][1] = "Java Archive Runner";
+        questions[7][2] = "Java Archive";
+        questions[7][3] = "Java Application Resource";
+        questions[7][4] = "Java Application Runner";
+
+        questions[8][0] = "Which of the following is a mutable class in java?";
+        questions[8][1] = "java.lang.StringBuilder";
+        questions[8][2] = "java.lang.Short";
+        questions[8][3] = "java.lang.Byte";
+        questions[8][4] = "java.lang.String";
+
+        questions[9][0] = "Which of the following option leads to the portability and security of Java?";
+        questions[9][1] = "Bytecode is executed by JVM";
+        questions[9][2] = "The applet makes the Java code secure and portable";
+        questions[9][3] = "Use of exception handling";
+        questions[9][4] = "Dynamic binding between objects";
+
+        //manually adding all the elements in the 2D arr [Answers]
+        answers[0][1] = "JDB";
+        answers[1][1] = "int";
+        answers[2][1] = "java.util package";
+        answers[3][1] = "Marker Interface";
+        answers[4][1] = "Heap memory";
+        answers[5][1] = "Remote interface";
+        answers[6][1] = "import";
+        answers[7][1] = "Java Archive";
+        answers[8][1] = "java.lang.StringBuilder";
+        answers[9][1] = "Bytecode is executed by JVM";
+
+        // creating the option panels
+
+        opt1 = new JRadioButton();
+        opt1.setBounds(170, 520, 700, 30);
+        opt1.setBackground(Color.WHITE);
+        opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        opt1.setBorderPainted(false);
+        add(opt1);
+
+        opt2 = new JRadioButton();
+        opt2.setBounds(170, 560, 700, 30);
+        opt2.setBackground(Color.WHITE);
+        opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
+        opt2.setBorderPainted(false);
+        add(opt2);
+
+        opt3 = new JRadioButton();
+        opt3.setBounds(170, 600, 700, 30);
+        opt3.setBackground(Color.WHITE);
+        opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
+        opt3.setBorderPainted(false);
+        add(opt3);
+
+        opt4 = new JRadioButton();
+        opt4.setBounds(170, 640, 700, 30);
+        opt4.setBackground(Color.WHITE);
+        opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
+        opt4.setBorderPainted(false);
+        add(opt4);
+
+        //Ensuring that user can only choose one option
+
+        groupOptions = new ButtonGroup();
+        groupOptions.add(opt1);
+        groupOptions.add(opt2);
+        groupOptions.add(opt3);
+        groupOptions.add(opt4);
+
+        // adding the next button
+
+        next = new JButton("Next");
+
+        next.setBounds(1100, 720, 200, 40);
+        next.setBorderPainted(false);
+        next.setBackground(new Color(83, 226, 245));
+        next.setForeground(Color.white);
+        next.setFont(new Font("San Serif", Font.PLAIN, 20));
+        // next.setEnabled(false); // user cannot go to the next panel until answering
+
+        next.addActionListener(this);
+
+        add(next);
+
+        // adding the submit button
+
+        submit = new JButton("Submit");
+
+        submit.setBounds(640, 720, 200, 40);
+        submit.setBorderPainted(false);
+        submit.setBackground(new Color(83, 226, 245));
+        submit.setForeground(Color.white);
+        submit.setFont(new Font("San Serif", Font.PLAIN, 20));
+        submit.setEnabled(false); // user cannot go to the next panel until answering
+
+        submit.addActionListener(this);
+
+        add(submit);
+
+        // adding 50/50 lifeline
+
+        hint = new JButton("Hint?");
+
+        hint.setBounds(170, 720, 200, 40);
+        hint.setBorderPainted(false);
+        hint.setBackground(new Color(83, 226, 245));
+        hint.setForeground(Color.white);
+        hint.setFont(new Font("San Serif", Font.PLAIN, 20));
+
+        hint.addActionListener(this);
+
+        add(hint);
+
+        start(count);
+
+        setVisible(true);
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+
+        String time = "Time left - " + timer + " seconds";  // ex. Time left - 30 seconds
+        g.setColor(Color.GREEN);
+        g.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+        if (timer > 0){
+            g.drawString(time, 1100, 500);
+        } else {
+            g.setColor(Color.RED);
+            g.drawString("Time's up", 1100, 500);
+        }
+
+        timer--;
+
+        try{
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception a){
+            a.printStackTrace();
+        }
+
+        if (ansGiven == 1){
+            ansGiven = 0;
+            timer = 15;
+        } else if (timer < 0){
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+
+            // checks if user answered or not
+            if (groupOptions.getSelection() == null){
+                userAns[count][0] = ""; // if user didn't select answer, it adds empty string to userAns[][];
+            } else {
+                userAns[count][0] = groupOptions.getSelection().getActionCommand(); // puts the answer in the userAns[][]
+            }
+            
+            count++;
+            start(count);
+        }
+    }
+
+    private void start(int count) {
+        quesNum.setText("" + (count + 1) + ".");
+        question.setText(questions[count][0]);
+
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]); // setting the values 
+                                                    // when the user chooses this option
+                                                    //s/he will store the answer in
+                                                    //userAns[count][0] = groupOptions.getSelection().getActionCommand();
+                                                    //inside paint(Graphics g)
+
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]); // repeating the same process for each option
+ 
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+ 
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupOptions.clearSelection(); // deselects the option after going to the next ques
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        opt1.setEnabled(true);
+        opt2.setEnabled(true);
+        opt3.setEnabled(true);
+        opt4.setEnabled(true);
+
+        if (ae.getSource() == next){
+            repaint();
+            
+            ansGiven = 1;
+            // checks if user answered or not
+            if (groupOptions.getSelection() == null){
+                userAns[count][0] = ""; // if user didn't select answer, it adds empty string to userAns[][];
+            } else {
+                userAns[count][0] = groupOptions.getSelection().getActionCommand(); // puts the answer in the userAns[][]
+            }
+
+            if (count == 8){
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            count++;
+            start(count);
+        } else if (ae.getSource() == hint){
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9){
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+        } else {
+
+        }
+    }
+    public static void main(String[] args) {
+        new Quiz();
+    }
+}
